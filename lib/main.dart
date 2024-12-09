92a5fa73cb6f2babe643f00ab9477610cc6ceea1
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-12-09 17:23:55 +0530

    login page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app_firebase/pages/login_page.dart';
+import 'package:flutter_chat_app_firebase/theme/light_mode.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: lightMode,
       home: const LoginPage(),
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,51 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_firebase/components/my_text_field.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text('Welcome back, you have been missed!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            //Email Address
+            MyTextField(
+              hintText: "Email",
+              obscuredText: false,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            //Password
+            MyTextField(
+              hintText: 'Password',
+              obscuredText: true,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
