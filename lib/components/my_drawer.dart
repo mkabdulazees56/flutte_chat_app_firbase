057cc535613dfdf60f8c422652336fe48dd97a82
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-12-10 12:26:21 +0530

    drawer navigations added

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,9 +1,17 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_firebase/auth/auth_service.dart';
+import 'package:flutter_chat_app_firebase/pages/settings%20_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
+
+  void logout() {
+    // get auth services
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +36,9 @@ class MyDrawer extends StatelessWidget {
                   child: ListTile(
                     leading: Icon(Icons.home),
                     title: Text('H O M E'),
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
                   ),
                 ),
                 Padding(
@@ -36,7 +46,14 @@ class MyDrawer extends StatelessWidget {
                   child: ListTile(
                     leading: Icon(Icons.settings),
                     title: Text('S E T T I N G S'),
-                    onTap: () {},
+                    onTap: () {
+                      // pop the drawer
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingsPage()));
+                    },
                   ),
                 ),
               ],
@@ -46,7 +63,7 @@ class MyDrawer extends StatelessWidget {
               child: ListTile(
                 leading: Icon(Icons.logout),
                 title: Text('L O G O U T'),
-                onTap: () {},
+                onTap: logout,
               ),
             ),
           ],
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app_firebase/auth/auth_service.dart';
 import 'package:flutter_chat_app_firebase/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    // get auth services
-    final _auth = AuthService();
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('home'),
-        actions: [
-          //logout button
-          IconButton(icon: Icon(Icons.logout), onPressed: logout),
-        ],
       ),
       drawer: MyDrawer(),
     );
