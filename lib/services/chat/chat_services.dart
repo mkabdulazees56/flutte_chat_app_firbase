701d43accb6496753439c2e7bd2548fdd16db825
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-12-11 10:44:23 +0530

    message model created and added related functions

--- lib/services/chat/chat_services.dart
+++ lib/services/chat/chat_services.dart
@@ -1,7 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chat_app_firebase/models/message.dart';
 
 class ChatService {
+// get the instance of firestore & auth
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+// get user stream
   Stream<List<Map<String, dynamic>>> getUsersStream() {
     return _firestore.collection('Users').snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
@@ -9,5 +15,46 @@ class ChatService {
         return user;
       }).toList();
     });
+  }
+
+  //send message to the user
+  Future<void> sendMessage(String receiverID, message) async {
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    //create a new message
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp);
+
+    //construct a chatroom id for two users
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    // add new message to the chatroom(database)
+    await _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .add(newMessage.toMap());
+
+    // get messages
+    Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+      List<String> ids = [userID, otherUserID];
+      ids.sort();
+      String chatRoomID = ids.join('_');
+
+      return _firestore
+          .collection('chat_rooms')
+          .doc(chatRoomID)
+          .collection('messages')
+          .orderBy('timestamp', descending: false)
+          .snapshots();
+    }
   }
 }
